936173a5e243a5fc0caf4dbcef93389194f5ee91
Leo Zuo <wzuo@yext.com>
2023-05-18 14:54:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,9 @@ headers = {"Authorization" : "Bearer " + 'token'}
 
 def prepGoogleEraser(file):
     prep = []
-    inputCSV = pd.read_csv(file, encoding = 'utf-8')
+    #inputCSV = csv.DictReader(file)
+    inputCSV = pd.read_csv(uploaded_file, encoding = 'utf-8')
+    inputCSV['Yext ID'] = inputCSV['Yext ID'].astype(int)
     st.write(inputCSV)
     for r in inputCSV:
         st.write(r)
