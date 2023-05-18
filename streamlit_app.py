b80e64b41fe1622bc353946ee4aab7a2552cc56d
Leo Zuo <wzuo@yext.com>
2023-05-18 14:44:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ headers = {"Authorization" : "Bearer " + 'token'}
 
 def prepGoogleEraser(file):
     prep = []
-    inputCSV = file.read().decode("utf-8-sig").encode("utf-8")
+    inputCSV = csv.DictReader(inputCSV).read().decode("utf-8-sig").encode("utf-8")
     for r in inputCSV:
         st.write(r)
         info = {}
