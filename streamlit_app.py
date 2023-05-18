3d7b34b30f2ba54977f0eefa475d2fa8f4bff5d8
Leo Zuo <wzuo@yext.com>
2023-05-18 14:11:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import streamlit as st
+import requests
+import csv
+import sys
+import time
+import json
 
 
 """
@@ -18,20 +23,3 @@ with st.echo(code_location='below'):
     google_file = st.file_uploader('Upload a CSV')
     google_operation = st.selectbox('Pick one', ['GET','POST','PATCH','DELETE'])
     print(google_file)
-
-    # Point = namedtuple('Point', 'x y')
-    # data = []
-
-    # points_per_turn = total_points / num_turns
-
-    # for curr_point_num in range(total_points):
-    #     curr_turn, i = divmod(curr_point_num, points_per_turn)
-    #     angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-    #     radius = curr_point_num / total_points
-    #     x = radius * math.cos(angle)
-    #     y = radius * math.sin(angle)
-    #     data.append(Point(x, y))
-
-    # st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-    #     .mark_circle(color='#0068c9', opacity=0.5)
-    #     .encode(x='x:Q', y='y:Q'))
