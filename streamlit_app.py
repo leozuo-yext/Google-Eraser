342668216ee76ccd21d4c5c77fb2c5a6da8c8cd2
Leo Zuo <wzuo@yext.com>
2023-05-22 11:14:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ def prepGoogleEraser(file):
     for row, r in inputCSV.iterrows():
         info = {}
         info["location"] = r["Yext ID"]
-        info["url"] = "https://mybusinessbusinessinformation.googleapis.com/v1/locations/%s?%s" % (r["GBP Location ID"][1:],params_str)
+        info["url"] = "https://mybusinessbusinessinformation.googleapis.com/v1/locations/%s?%s" % r["GBP Location ID"][1:] , params_str
         prep.append(info)
     return prep
 
