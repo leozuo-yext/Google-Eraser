c0d464f4d4279c94bf9ee8df31ba736e7e6a2086
Leo Zuo <wzuo@yext.com>
2023-05-22 10:52:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,24 +18,11 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-"""
-### Please Pick a Google Endpoint
-"""
-google_endpoint = st.selectbox('Google Endpoint', options = ['Business Info','Place Actions','Attributes','Media'])
-if google_endpoint == 'Business Info':
-    """
-    ### Please enter the updateMasks, seperated by commas
-    """
-    params_str = '?updateMask=' + st.text_input('Google Update Masks')
-
-else:
-    params_str = ""
-
 
 """
-### Please Pick An Operation
+### Please enter the updateMasks, seperated by commas
 """
-google_operation = st.selectbox('Google Operation', options = ['GET','POST','PATCH','DELETE'])
+params_str = '?updateMask=' + st.text_input('Google Update Masks')
 
 
 """
